d2e3cf8fc7ec05c806bd5a968ba7c6dab646afc9
Michael Pellegrini <michael@cappers.ai>
2024-09-13 13:24:12 -0700

    Adds very simple styling to predictions page

--- src/routes/ncaaf/events/[eventId]/+page.svelte
+++ src/routes/ncaaf/events/[eventId]/+page.svelte
@@ -6,30 +6,56 @@
     const prediction: PredictorResponse = data.prediction;
 </script>
 
+<style>
+    h1 {
+        color: blue;
+        font-size: 24px;
+    }
+
+    .prediction-container {
+        background-color: #f0f0f0;
+        padding: 20px;
+        border-radius: 8px;
+    }
+
+    .predictions-table {
+        border: 1px solid #ccc;
+        padding: 10px;
+    }
+
+    .predictions-table th,
+    .predictions-table td {
+        margin: 10px;
+        padding: 10px;
+    }
+</style>
+
 {#if data}
-    <h1>{prediction.name}</h1>
-    <h2>Predictions</h2>
-    <table cellpadding="5" cellspacing="5">
-    <thead>
-        <tr>
-            <th>Type</th>
-            <th>Away Team</th>
-            <th>Home Team</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Win Probability</td>
-            <td>{prediction.awayTeam.statistics[0].displayValue}%</td>
-            <td>{prediction.homeTeam.statistics[0].displayValue}%</td>
-        </tr>
-        <tr>
-            <td>Predicted Point Difference</td>
-            <td>{prediction.awayTeam.statistics[6].displayValue}</td>
-            <td>{prediction.homeTeam.statistics[6].displayValue}</td>
-        </tr>
-    </tbody>
-    </table>
+    <div class="prediction-container">
+        <h1>{prediction.name}</h1>
+        <h2>Predictions</h2>
+        <table class="predictions-table">
+        <thead>
+            <tr>
+                <th>Type</th>
+                <th>Away Team</th>
+                <th>Home Team</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Win Probability</td>
+                <td>{prediction.awayTeam.statistics[0].displayValue}%</td>
+                <td>{prediction.homeTeam.statistics[0].displayValue}%</td>
+            </tr>
+            <tr>
+                <td>Predicted Point Difference</td>
+                <td>{prediction.awayTeam.statistics[6].displayValue}</td>
+                <td>{prediction.homeTeam.statistics[6].displayValue}</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
 {:else}
     <h1>{prediction === undefined ? "Loading..." : "Error loading prediction"}</h1>
 {/if}
